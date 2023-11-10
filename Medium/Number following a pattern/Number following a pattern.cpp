da2c0a3a4c98e615f13a0a201087492cd4006c70
Ashish Kumar <ashishkumar11125@hotmail.com>
2023-11-10 23:41:41 +0530

    Create Number following a pattern.cpp

---
+++ Medium/Number following a pattern/Number following a pattern.cpp
@@ -0,0 +1,25 @@
+class Solution{   
+public:
+    string printMinNumberForPattern(string S){
+        // code here 
+        int n = S.length(); // Get the length of the input string.
+        string result; // Initialize a string to store the result.
+        stack<int> stk; // Initialize a stack to keep track of numbers.
+
+        // Iterate through the input string and process each character.
+        for (int i = 0; i <= n; i++) {
+            stk.push(i + 1); // Push the current number onto the stack.
+
+            // Check if the current character is 'I' or the end of the string.
+            if (i == n || S[i] == 'I') {
+                // Pop elements from the stack and append them to the result string.
+                while (!stk.empty()) {
+                    result += to_string(stk.top());
+                    stk.pop();
+                }
+            }
+        }
+
+        return result; // Return the final result string.
+    }
+};
