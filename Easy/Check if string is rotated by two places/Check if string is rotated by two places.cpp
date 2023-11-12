d32973aceb54d5432fc6b5ca9fae10bfa79ce6e5
Ashish Kumar <ashishkumar11125@hotmail.com>
2023-11-12 22:06:42 +0530

    Create Check if string is rotated by two places.cpp

---
+++ Easy/Check if string is rotated by two places/Check if string is rotated by two places.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+    public:
+    //Function to check if a string can be obtained by rotating
+    //another string by exactly 2 places.
+    bool isRotated(string str1, string str2)
+    {
+        // Your code here
+        string left = str2.substr(2) + str2.substr(0, 2);
+        string right = str2.substr(str2.size() - 2) + str2.substr(0, str2.size() - 2);
+        return str1 == left || str1 == right;
+        
+    }
+
+};
