6febff73a0ddac742d065fd48b93f84ced6e4b94
Ashish Kumar <101413766+ashishkumar11125@users.noreply.github.com>
2023-11-21 23:22:06 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Determine if Two Trees are Identical/determine-if-two-trees-are-identical.cpp
@@ -0,0 +1,152 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int x){
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+/* A binary tree node
+
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+
+
+   
+class Solution {
+public:
+    // Function to check if two trees are identical.
+    bool isIdentical(Node *r1, Node *r2) {
+        // Check if both nodes are NULL, indicating the end of the tree
+        if (!r1 and !r2) {
+            return true;
+        }
+        // If one of the nodes is NULL and the other is not, trees are not identical
+        else if ((r1 and !r2) or (!r1 and r2)) {
+            return false;
+        }
+
+        // If data of both nodes is equal, check the left and right subtrees
+        if (r1->data == r2->data) {
+            // Recursively check the left subtree and logical AND the result
+            bool leftSubtree = isIdentical(r1->left, r2->left);
+
+            // Recursively check the right subtree and logical AND the result
+            bool rightSubtree = isIdentical(r1->right, r2->right);
+
+            // Return the logical AND of the results for both subtrees
+            return leftSubtree and rightSubtree;
+        } else {
+            // If data is not equal, trees are not identical
+            return false;
+        }
+    }
+};
+
+
+//{ Driver Code Starts.
+
+// Function to Build Tree
+Node *buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+int main() {
+    int tc;
+    scanf("%d ", &tc);
+    while (tc--) {
+        string str, str1;
+        getline(cin, str);
+        Node *rootA = buildTree(str);
+        getline(cin, str1);
+        Node *rootB = buildTree(str1);
+        Solution ob;
+        if (ob.isIdentical(rootA, rootB)) {
+            cout << "Yes\n";
+        } else {
+            cout << "No\n";
+        }
+    }
+    return 0;
+}
+// } Driver Code Ends
