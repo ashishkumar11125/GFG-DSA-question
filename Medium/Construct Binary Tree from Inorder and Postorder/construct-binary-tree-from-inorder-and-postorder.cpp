3918acb3368a3450025abad1be7f4878407f128c
Ashish Kumar <101413766+ashishkumar11125@users.noreply.github.com>
2024-05-04 23:05:38 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Construct Binary Tree from Inorder and Postorder/construct-binary-tree-from-inorder-and-postorder.cpp
@@ -0,0 +1,86 @@
+//{ Driver Code Starts
+/* program to construct tree using inorder and postorder traversals */
+#include <bits/stdc++.h>
+using namespace std;
+
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child */
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x) {
+        data = x;
+        left = right = NULL;
+    }
+};
+
+/* This funtcion is here just to test buildTreeUtil() */
+void preOrder(Node* node) {
+    if (node == NULL) return;
+
+    /* then print the data of node */
+    printf("%d ", node->data);
+
+    /* first recur on left child */
+    preOrder(node->left);
+
+    /* now recur on right child */
+    preOrder(node->right);
+}
+
+
+// } Driver Code Ends
+/* Tree node structure
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};*/
+
+class Solution
+{
+    public:
+
+    //Function to return a tree created from postorder and inoreder traversals.
+    Node *buildTree(int in[], int post[], int n) {
+    if(n<=0) return nullptr;
+    
+    Node* root= new Node(post[n-1]);
+    int i;
+    for(i=0; i<n; i++) if(post[n-1]==in[i]) break;
+    
+    root->left= buildTree(in,post,i);
+    root->right=buildTree(in+i+1,post+i,n-i-1);
+    return root;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t, n;
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        int in[n], post[n];
+        for (int i = 0; i < n; i++) cin >> in[i];
+        for (int i = 0; i < n; i++) cin >> post[i];
+        Solution obj;
+        Node* root = obj.buildTree(in, post, n);
+        preOrder(root);
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
