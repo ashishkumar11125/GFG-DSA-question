59fcb6fd2240e6e7bdc4f6471d1e347aab00d46c
Ashish Kumar <101413766+ashishkumar11125@users.noreply.github.com>
2024-08-16 19:38:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Merge Without Extra Space/merge-without-extra-space.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+        //Function to merge the arrays.
+        void merge(long long nums1[], long long nums2[], int m, int n) 
+        {
+            int last = m - 1;
+            int first = 0;
+            
+            while(last >= 0 && first < n)
+            {
+                if(nums1[last] >= nums2[first])
+                {
+                    swap(nums1[last],nums2[first]);
+                }
+                last--;
+                first++;
+            }
+            
+            sort(nums1,nums1 + m);
+            sort(nums2, nums2 + n);
+        } 
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{ 
+	
+	int T;
+	cin >> T;
+	
+	while(T--){
+	    int n, m;
+	    cin >> n >> m;
+	    
+	    long long arr1[n], arr2[m];
+	    
+	    for(int i = 0;i<n;i++){
+	        cin >> arr1[i];
+	    }
+	    
+	    for(int i = 0;i<m;i++){
+	        cin >> arr2[i];
+	    }
+	    Solution ob;
+	    ob.merge(arr1, arr2, n, m); 
+
+        for (int i = 0; i < n; i++) 
+            cout<<arr1[i]<<" "; 
+        
+       
+	    for (int i = 0; i < m; i++) 
+		    cout<<arr2[i]<<" "; 
+	    
+	    cout<<endl;
+	}
+
+	return 0; 
+} 
+
+// } Driver Code Ends
