2006f7a4034205f424a2ace8017a0873d0b85511
Ashish Kumar <101413766+ashishkumar11125@users.noreply.github.com>
2024-08-26 23:47:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Wildcard Pattern Matching/wildcard-pattern-matching.cpp
+++ Difficulty: Hard/Wildcard Pattern Matching/wildcard-pattern-matching.cpp
@@ -6,27 +6,37 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    /*You are required to complete this method*/
-    int f(int i, int j, string &pattern, string &str, vector<vector<int>> &dp) {
-        if (i < 0 && j < 0) return 1;
-        if (i < 0 && j >= 0) return 0;
-        if (i >= 0 && j < 0) {
-            for (int ii = i; ii >= 0; ii--) {
-                if (pattern[ii] != '*') return false;
+    int helper(int i,int j,string &str,string &pat,vector<vector<int>>&dp){
+        if(i<0 && j<0) return true;
+        
+        if(j<0 && i>=0) return false;
+        
+        if(i<0 && j>=0){
+            for(int k=j;k>=0;k--){
+                if(pat[k]!='*') return false;
             }
+            
             return true;
         }
-        if (dp[i][j] != -1) return dp[i][j];
-        if (pattern[i] == str[j] || pattern[i] == '?') return dp[i][j] = f(i-1, j-1, pattern, str, dp);
-        if (pattern[i] == '*') return dp[i][j] = f(i-1, j, pattern, str, dp) || f(i, j-1, pattern, str, dp);
-        return dp[i][j] = false;
+        
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        int cond1=false;
+        if(pat[j]=='*'){
+            return dp[i][j]=helper(i-1,j,str,pat,dp) || helper(i,j-1,str,pat,dp);
+        }
+        
+        else if(pat[j]=='?' || pat[j]==str[i])
+            return dp[i][j]=helper(i-1,j-1,str,pat,dp);
+        
+        return dp[i][j]=false;
     }
-
+    
     int wildCard(string pattern, string str) {
-        int n = pattern.length();
-        int m = str.length();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return f(n-1, m-1, pattern, str, dp);
+        // code here
+        int pat=pattern.size(),s=str.size();
+        vector<vector<int>>dp(s,vector<int>(pat,-1));
+        return helper(s-1,pat-1,str,pattern,dp);
     }
 };
 
