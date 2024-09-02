939662f0a1bede4a4f64f3fc299ffe9b471e1c95
Ashish Kumar <101413766+ashishkumar11125@users.noreply.github.com>
2024-09-02 19:16:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Minimum Cost Path/minimum-cost-path.cpp
+++ Difficulty: Hard/Minimum Cost Path/minimum-cost-path.cpp
@@ -10,35 +10,43 @@ class Solution
     public:
     //Function to return the minimum cost to react at bottom
 	//right cell from top left cell.
-    int minimumCostPath(vector<vector<int>>& grid) 
+   int minimumCostPath(vector<vector<int>>& grid) 
     {
-        // Code here
-        int n=grid.size();
-        int m=grid[0].size();
-        vector<vector<int>> vis(grid.size(),vector<int>(grid[0].size(),1e9));
-        vis[0][0]=grid[0][0];
-        set<pair<int,pair<int,int>>>s;
-        s.insert({grid[0][0],{0,0}});
-        int r[4]={-1,1,0,0};
-        int c[4]={0,0,1,-1};
-        while (!s.empty()){
-            auto it=s.begin();
-            s.erase(it);
-            int wt=it->first;
-            int row=it->second.first;
-            int col=it->second.second;
-            for (int k=0;k<4;k++){
-                int nr=row+r[k];
-                int nc=col+c[k];
-                if (nr>=0 and nc>=0 and nr<n and nc<m ){
-                    if (wt+grid[nr][nc]<vis[nr][nc]){
-                        vis[nr][nc]=wt+grid[nr][nc];
-                        s.insert({wt+grid[nr][nc],{nr,nc}});
-                    }
-                }
+        int n = grid.size(), m = grid[0].size();
+        if(n == 1 && m == 1) return grid[0][0];
+        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
+        dist[0][0] = grid[0][0];
+        
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        pq.push({grid[0][0], 0});
+        
+        while(!pq.empty()) {
+            int curr = pq.top().first;
+            int i = pq.top().second/m, j = pq.top().second%m;
+            pq.pop();
+            
+            if(i + 1 < n && dist[i + 1][j] > curr + grid[i + 1][j]) {
+                dist[i + 1][j] = curr + grid[i + 1][j];
+                int nex = (i + 1)*m + j;
+                pq.push({dist[i + 1][j], nex});
+            }
+            if(i - 1 >= 0 && dist[i - 1][j] > curr + grid[i - 1][j]) {
+                dist[i - 1][j] = curr + grid[i - 1][j];
+                int nex = (i - 1)*m + j;
+                pq.push({dist[i - 1][j], nex});
+            }
+            if(j + 1 < m && dist[i][j + 1] > curr + grid[i][j + 1]) {
+                dist[i][j + 1] = curr + grid[i][j + 1];
+                int nex = (i)*m + j + 1;
+                pq.push({dist[i][j + 1], nex});
+            }
+            if(j - 1 >= 0 && dist[i][j - 1] > curr + grid[i][j - 1]) {
+                dist[i][j - 1] = curr + grid[i][j - 1];
+                int nex = (i)*m + j - 1;
+                pq.push({dist[i][j - 1], nex});
             }
         }
-        return vis[n-1][m-1];
+        return dist[n - 1][m - 1];
     }
 };
 
