a8825741c07eb65bbd8181b46c36246c03b5a97a
Ashish Kumar <101413766+ashishkumar11125@users.noreply.github.com>
2024-09-30 00:17:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge two BST 's/merge-two-bst-s.cpp
+++ Difficulty: Medium/Merge two BST 's/merge-two-bst-s.cpp
@@ -94,35 +94,31 @@ struct Node {
 */
 class Solution {
   public:
-    void findAns(Node *root1, Node *root2, vector<int> &ans){
-        if(root1== NULL && root2 == NULL){
-            return;
-        }
-        if(root1 == NULL && root2!=NULL){
-            findAns(root1,root2->left,ans);
-            ans.push_back(root2->data);
-            findAns(root1,root2->right,ans);
-        }else if(root2 == NULL && root1!=NULL){
-            findAns(root1->left,root2,ans);
-            ans.push_back(root1->data);
-            findAns(root1->right,root2,ans);
-        }else{
-            findAns(root1->left,root2->left,ans);
-             ans.push_back(root1->data);
-             ans.push_back(root2->data);
-             findAns(root1->right,root2->right,ans);
-            
-        }
-        
+    // Function to return a list of integers denoting the node
+    // values of both the BST in a sorted order.
+    void inorder(Node* root,vector<int>&ans){
+        if(root==NULL)return;
+        inorder(root->left,ans);
+        ans.push_back(root->data);
+        inorder(root->right,ans);
     }
     vector<int> merge(Node *root1, Node *root2) {
         // Your code here
-        vector<int> ans;
-         findAns(root1,root2,ans);
-         sort(ans.begin(),ans.end());
-         return ans;
+        vector<int>ans;
+        inorder(root1,ans);
+        inorder(root2,ans);
+        sort(ans.begin(),ans.end());
+        return ans;
     }
 };
+
+
+
+
+
+
+
+
 
 //{ Driver Code Starts.
 int main() {
