5570524b7b0fdab22b6453e1b2a03f1127a27b58
Ashish Kumar <101413766+ashishkumar11125@users.noreply.github.com>
2025-01-17 23:50:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Product array puzzle/product-array-puzzle.cpp
+++ Difficulty: Easy/Product array puzzle/product-array-puzzle.cpp
@@ -1,6 +1,4 @@
 //{ Driver Code Starts
-// Initial template for C++
-
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -10,61 +8,68 @@ using namespace std;
 
 class Solution {
   public:
-    // nums: given vector
-    // return the Product vector P that hold product except self at each index
-    vector<long long int> productExceptSelf(vector<long long int>& nums) {
-
+    vector<int> productExceptSelf(vector<int>& arr) {
         // code here
-        vector<long long int>ans;
-        long long int sum =1;
-        int temp = 0;
-        int n = nums.size();
-        for (int i=0;i < nums.size();i++){
-            if (nums[i]==0){
-                temp++;
-                if (temp >1) break;
-            }
+        vector<int> vec(arr.size());
+        int product = 1;
+        int zeroCount = 0;
+        
+        for(int i=0; i<arr.size(); i++) {
+            if(arr[i] == 0) zeroCount++;
+            else product *= arr[i];
         }
-        for(int i=0;i<n;i++)if(nums[i]!=0)sum*=nums[i];
-        for(int i=0;i<n;i++){
-            if(nums[i]!=0){
-                if(temp>=1)ans.push_back(0);
-                else{
-                    ans.push_back(sum/(long long int)nums[i]);
-                }
+        
+        //case 1 - when zeroCount is more than 1
+        if(zeroCount > 1) return vector<int>(arr.size(), 0);
+        
+        //case 2 - when zeroCount is 1
+        if(zeroCount == 1) {
+            for(int i=0; i<arr.size(); i++) {
+                if(arr[i] == 0) vec[i] = product;
+                else vec[i] = 0;
             }
-            else{
-                if(temp>1)ans.push_back(0);
-                else ans.push_back(sum);
-            }
+            return vec;
         }
-        return ans;
+        
+        //case 3
+        for(int i=0; i<arr.size(); i++) {
+            vec[i] = product/arr[i];
+        }
+        return vec;
     }
 };
 
 
 //{ Driver Code Starts.
+
 int main() {
-    int t; // number of test cases
+
+    int t;
     cin >> t;
+    cin.ignore();
+
     while (t--) {
-        int n; // size of the array
-        cin >> n;
-        vector<long long int> arr(n), vec(n);
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
 
-        for (int i = 0; i < n; i++) // input the array
-        {
-            cin >> arr[i];
+        while (ss >> number) {
+            arr.push_back(number);
         }
-        Solution obj;
-        vec = obj.productExceptSelf(arr); // function call
 
-        for (int i = 0; i < n; i++) // print the output
-        {
-            cout << vec[i] << " ";
+        Solution obj;
+        vector<int> res = obj.productExceptSelf(arr);
+
+        for (int i = 0; i < res.size(); i++) {
+            cout << res[i] << " ";
         }
         cout << endl;
+        cout << "~\n";
     }
+
     return 0;
 }
+
 // } Driver Code Ends
