794483783381b189134fb9be90f2a48ed64f0b04
Ashish Kumar <101413766+ashishkumar11125@users.noreply.github.com>
2025-01-31 17:35:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/N-Queen Problem/nqueen-problem.cpp
+++ Difficulty: Hard/N-Queen Problem/nqueen-problem.cpp
@@ -4,90 +4,76 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
 // User function Template for C++
 
-class Solution{
-public:
-   bool isValid(int i,int j,vector<vector<int>>&matrix){
-        int x = i, y = j;
-        int n = matrix.size();
-        
-        // Across Column
-        for(int j=0;j<n;j++){
-            if (matrix[x][j]!=0) return false;    
-        }
-        
-        // Across Row
-        for(int i=0;i<n;i++){
-            if (matrix[i][y]!=0) return false;    
-        }
-        
-        // Across Both Diagonals
-        for(int i=x,j=y;i<n && j<n;i++,j++){
-            if (matrix[i][j]!=0) return false;    
-        }
-        for(int i=x,j=y;i>=0 && j>=0;i--,j--){
-            if (matrix[i][j]!=0) return false;    
-        }
-        for(int i=x,j=y;i<n && j>=0;i++,j--){
-            if (matrix[i][j]!=0) return false;    
-        }
-        for(int i=x,j=y;i>=0 && j<n;i--,j++){
-            if (matrix[i][j]!=0) return false;    
-        }
-        
-        return true;
-    }
-    void solve(int j,vector<vector<int>>&matrix,vector<int>&temp,vector<vector<int>>&ans){
-        int n = matrix.size();
-        if (j==n){
-            ans.push_back(temp);
+class Solution {
+  public:
+    int size;
+    vector<int>diagup,diagdown,horizontal;
+    
+    void helper(int col, vector<int> &ds, vector<vector<int>> &ans) {
+        if (col >= size) {
+            ans.push_back(ds);
             return;
         }
-        
-        for(int i=0;i<n;i++){
-            if (isValid(i,j,matrix)){
-                temp.push_back(i+1);
-                matrix[i][j] = i+1;
-                solve(j+1,matrix,temp,ans);
-                matrix[i][j] = 0;
-                temp.pop_back();
+    
+        for (int r = 0; r < size; r++) {
+            if (!horizontal[r] && !diagdown[r + col] && !diagup[r - col + (size - 1)]) {
+                horizontal[r] = 1;
+                diagdown[r + col] = 1;
+                diagup[r - col + (size - 1)] = 1;
+                ds.push_back(r + 1);
+                helper(col + 1, ds, ans);
+                ds.pop_back();
+                horizontal[r] = 0;
+                diagdown[r + col] = 0;
+                diagup[r - col + (size - 1)] = 0;
             }
         }
     }
+    
     vector<vector<int>> nQueen(int n) {
         // code here
-        vector<vector<int>>ans;
-        vector<int>temp;
-        vector<vector<int>>matrix(n,vector<int>(n,0));
-        solve(0,matrix,temp,ans);
+        size = n;
+        vector<vector<int>> ans;
+        vector<int> ds;
+        diagup.resize(2 * n - 1, 0);
+        diagdown.resize(2 * n - 1, 0);
+        horizontal.resize(n, 0);
+        helper(0, ds, ans);
         return ans;
     }
+    
 };
 
 //{ Driver Code Starts.
 
-int main(){
+int main() {
     int t;
-    cin>>t;
-    while(t--){
+    cin >> t;
+    while (t--) {
         int n;
-        cin>>n;
-        
+        cin >> n;
+
         Solution ob;
         vector<vector<int>> ans = ob.nQueen(n);
-        if(ans.size() == 0)
-            cout<<-1<<"\n";
+        if (ans.size() == 0)
+            cout << -1 << "\n";
         else {
-            for(int i = 0;i < ans.size();i++){
-                cout<<"[";
-                for(int u: ans[i])
-                    cout<<u<<" ";
-                cout<<"] ";
+            sort(ans.begin(), ans.end());
+            for (int i = 0; i < ans.size(); i++) {
+                cout << "[";
+                for (int u : ans[i])
+                    cout << u << " ";
+                cout << "] ";
             }
-            cout<<endl;
+            cout << endl;
         }
+
+        cout << "~"
+             << "\n";
     }
     return 0;
 }
