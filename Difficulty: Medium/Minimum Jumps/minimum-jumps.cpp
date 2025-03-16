eb7779d32e0a7bd4d89d962896230f48d2b99a33
Ashish Kumar <101413766+ashishkumar11125@users.noreply.github.com>
2025-03-16 23:40:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Jumps/minimum-jumps.cpp
+++ Difficulty: Medium/Minimum Jumps/minimum-jumps.cpp
@@ -4,22 +4,35 @@ using namespace std;
 
 
 // } Driver Code Ends
-// Function to return minimum number of jumps to end of array
 
 class Solution {
   public:
     int minJumps(vector<int>& arr) {
-        // Your code here
-        int n = arr.size(), maxInd = 0, nextMaxInd = arr[0], res = 0;
-        for ( int i = 0; i<n-1; i++ ){
-            if ( maxInd == i ){
-                maxInd = max(nextMaxInd, i+arr[i]);
-                nextMaxInd = 0;
-                res++;
-            } else nextMaxInd = max(nextMaxInd, i+arr[i]);
-        } return (maxInd >= n-1) ? res : -1;
+        // code here
+         // code here
+        if(arr[0] == 0)
+        return -1;
+        int n = arr.size();
+        vector<int> dp(n,INT_MAX);
+        dp[0] = 0;
+        for(int i = 1;i<n;i++)
+        {
+            for(int j = i-1;j>=0;j--)
+            {
+                if(arr[j] + j >= i && dp[j] != INT_MAX)
+                {
+                    dp[i] = min(dp[i], 1+dp[j]);
+                }
+            }
+        }
+        int ans = dp[n-1];
+        if(ans == INT_MAX)
+        return -1;
+        
+        return ans;
     }
 };
+
 
 
 //{ Driver Code Starts.
@@ -40,7 +53,7 @@ int main() {
             arr.push_back(number);
         }
         Solution obj;
-        cout << obj.minJumps(arr) << endl;
+        cout << obj.minJumps(arr) << endl << "~\n";
     }
     return 0;
 }
