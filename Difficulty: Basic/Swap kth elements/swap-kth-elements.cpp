7bc8d3ee9e9ea5022ab7ccb9efb1e9b2d26f00f6
Ashish Kumar <101413766+ashishkumar11125@users.noreply.github.com>
2025-05-23 03:27:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Swap kth elements/swap-kth-elements.cpp
@@ -0,0 +1,12 @@
+
+class Solution {
+  public:
+    void swapKth(vector<int> &arr, int k) {
+        // code here
+        int n = arr.size();
+        swap(arr[k-1],arr[n-k]);
+        
+    }
+};
+
+
