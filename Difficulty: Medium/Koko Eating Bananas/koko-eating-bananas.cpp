43bcf1378ab311b74f089896e02920c99b5c1a0f
Ashish Kumar <101413766+ashishkumar11125@users.noreply.github.com>
2025-06-13 22:53:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Koko Eating Bananas/koko-eating-bananas.cpp
+++ Difficulty: Medium/Koko Eating Bananas/koko-eating-bananas.cpp
@@ -1,47 +1,42 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
-    int Solve(vector<int>& arr, int k) {
-        // Code here
-        int l=1,h=1000000000;
-        while(l<h){
-            int mid=(l+h)/2,res=0;
-            for(int x:arr)res+=(x+mid-1)/mid;
-            if(res>k)l=mid+1;
-            else h=mid;
+    bool possible(vector<int> &arr,int n, int k,int mid){
+        int temp=k;
+        for(int i=0;i<n;i++){
+            int quant = arr[i]/mid;
+            int rem = arr[i]%mid;
+            
+            if(quant ==0)   temp-=1;
+            else{
+                if(rem!=0)
+                    temp -= (quant+1);
+                else{
+                    temp -= quant;
+                }
+            }
+            if(temp<0)
+                return false;
         }
-        return l;
+        return true;
+        
+    }
+    int kokoEat(vector<int>& arr, int k) {
+        // Code here
+        int end = *max_element(arr.begin(),arr.end());
+        int n = arr.size();
+        int start = 1;
+        int ans=-1;
+        
+        while(start<=end){
+            int mid = start+ (end-start)/2;
+            if(possible(arr,n,k,mid)){
+                ans = mid;
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        return ans;
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    string ts;
-    getline(cin, ts);
-    int t = stoi(ts);
-    while (t--) {
-
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        string ks;
-        getline(cin, ks);
-        int k = stoi(ks);
-        Solution ob;
-
-        cout << ob.Solve(arr, k);
-        cout << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
