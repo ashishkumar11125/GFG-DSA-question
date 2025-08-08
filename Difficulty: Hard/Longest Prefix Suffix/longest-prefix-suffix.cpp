d9dd5127caeaf09203151d79ee86ffd752edd65e
Ashish Kumar <101413766+ashishkumar11125@users.noreply.github.com>
2025-08-08 20:40:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Longest Prefix Suffix/longest-prefix-suffix.cpp
+++ Difficulty: Hard/Longest Prefix Suffix/longest-prefix-suffix.cpp
@@ -1,64 +1,27 @@
-//{ Driver Code Starts
-// Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
-// User function template for C++
-
-class Solution{
-  public:		
-	int lps(string s) {
-	    // Your code goes here
-	      
-    int n = s.length();
-    int i = 1;
-    int j = 0;
- 
-    int array[n] = {0};
-    while(i < n){
-        if(s[i] == s[j]){
-            array[i] = j+1;
-            i++;
-            j++;
-        }
-        else{
-            if(j != 0){
-               j = array[j-1]; 
-            }
-            else{
-                i++;
-            }
-        }
+class Solution {
+  public:
+    int getLPSLength(string &s) {
+        // code here
+        int n = s.size();
+                int maxi =0;
+                vector<int>idx;
+                 
+                for(int i=n-2;i>=0;i--){
+                      if (s[i]==s[n-1]){
+                             
+                                    //   cout<<i<<" "<<n-i<<" ";  /                
+                         string k = s.substr(0,i+1);
+                         string m = s.substr(n-i-1,i+1);
+                         
+                         if (k==m){
+                              return i+1;
+                              
+                         }
+                               
+                            
+                      }
+                } 
+                
+                return 0;
     }
-    return array[n-1];
-	}
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-
-    ios_base::sync_with_stdio(0);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    int t;
-    cin >> t;
-    while (t--) {
-        string str;
-        cin >> str;
-
-        Solution ob;
-
-        cout << ob.lps(str) << "\n";
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
